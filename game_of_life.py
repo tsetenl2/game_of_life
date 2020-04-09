3953dfd94ff2af13d0513cfac0435bbb4ad9d6ac
TLX3 <tdl2116@columbia.edu>
2020-04-09 10:50:56 -0400

    Quick implementation following wiki description

---
+++ game_of_life.py
@@ -0,0 +1,58 @@
+from random import random
+from sys import stdout
+from time import sleep
+
+class Game:
+  def __init__(self, size):
+    self.size = size
+    self.grid = self.create_grid()
+
+  def stringify(self):
+    rep = '\n'
+    for row in self.grid:
+      for val in row:
+        rep += val
+        rep += ' '
+      rep += '\n'
+    return rep
+
+  def create_grid(self):
+    n = self.size
+    grid = []
+    for i in range(n):
+      row = [''] * n
+      grid.append(row)
+      for j in range(n):
+        if random() <= 0.5:
+          grid[i][j] = '*'
+    return grid
+
+  def check_neighbors(self, i, j):
+    n = self.size
+    count = 0
+    coords = [(i + 1, j), (i + 1, j + 1), (i, j + 1), (i - 1, j + 1), 
+              (i - 1, j), (i - 1, j - 1), (i, j - 1), (i + 1, j - 1)]
+    coords = list(filter(lambda pair: pair[0] >= 0 and pair[0] < n and pair[1] >= 0 and pair[1] < n, coords))
+    for (x, y) in coords:
+      if self.grid[x][y] == '*':
+        count += 1
+    return count
+
+  def update(self):
+    n = self.size
+    for i in range(n):
+      for j in range(n):
+        count = self.check_neighbors(i, j)
+        if count < 2 or count > 3:
+          self.grid[i][j] = ''
+        elif count == 3:
+          self.grid[i][j] = '*'
+
+
+if __name__ == '__main__':
+  game = Game(10)
+  for i in range(100):
+    sleep(0.1)
+    stdout.write(game.stringify())
+    stdout.flush()
+    game.update()
